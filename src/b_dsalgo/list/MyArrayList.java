fb987217633e2bcaec2398cf28528eff94202ee0
frfn <flexer.fadrigalan@gmail.com>
2021-03-22 20:43:17 -0400

    starting linked list!

--- src/b_dsalgo/list/MyArrayList.java
+++ src/b_dsalgo/list/MyArrayList.java
@@ -129,7 +129,7 @@ public class MyArrayList<X> {
 
 }
 
-class Main {
+class MyArrayListMain {
     public static void main(String[] args) {
         MyArrayList<Integer> list = new MyArrayList<Integer>(); // instantiate list
 
---
+++ src/b_dsalgo/list/MyLinkedList.java
@@ -0,0 +1,23 @@
+package b_dsalgo.list;
+
+public class MyLinkedList {
+
+}
+
+class Node<X> {
+
+    private X item;
+    private int size;
+
+    public Node(X item) {
+        this.item = item;
+        size = 0;
+    }
+
+}
+
+class MyLinkedListMain {
+    public static void main(String[] args){
+
+    }
+}

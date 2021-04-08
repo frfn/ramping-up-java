5a1858d1acbbbcb45992f12c91640e97c9556564
frfn <flexer.fadrigalan@gmail.com>
2021-04-08 15:29:20 -0400

    relearned how mergesort works!

--- src/c_sort/mergesort/mergesort.java
+++ src/c_sort/mergesort/mergesort.java
@@ -1,26 +1,80 @@
 package c_sort.mergesort;
 
 public class mergesort {
-}
 
-/*
-int[] merged = new int[m + n];
-        int i, j, k;
-        i=j=k=0;
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 1, 8, 9, 2};
+        mergeSort(arr);
 
-        while(i != m && j != n) {
-            if(nums1[i] <= nums2[j]){
-                merged[k++] = nums1[i++];
-            } else {
-                merged[k++] = nums2[j++];
+        for(int num: arr) System.out.print(num + ", ");
+    }
+
+    // 5 3 1 8 9 2
+    // middle = 6 / 2 = 3
+    // left[3] | right[6-3] = right[3]
+    // left[] = { 5, 3, 1 }
+    // right[] { 8, 9, 2 }
+
+    // middle = 3 / 2 = 1.5 = 1
+    // left[1] | right[3-1] = right[2]
+    // left[] = { 5 }
+    // right[] = { 3 , 1 }
+
+    // finish a call in the call stack since left length == 1
+    // continue to mergeSort(right);
+    // arr = { 3, 1 }
+    // left[] = { 3 }
+    // right[] = { 1 }
+    // merge( {3,1} , {3} , {1} ) == 1 3
+    // finish a call in the call stack
+    // arr = { 5, 1, 3 }
+    // merge( {5, 1, 3}, {5}, {1, 3} ) == 1 3 5
+
+    // move on to right branch!!
+
+    public static void mergeSort(int[] arr) {
+        if(arr.length == 1) {
+            return;
+        }
+
+        int middle = arr.length / 2;
+        int[] left = new int[middle];
+        int[] right = new int[arr.length - middle];
+
+        {
+            // for left
+            for(int i = 0; i < left.length; i++) {
+                left[i] = arr[i];
             }
         }
 
-        while(i != m) {
-            merged[k++] = nums1[i++];
+        {
+            // for right
+            for(int i = 0; i < right.length; i++) {
+                right[i] = arr[i + middle];
+            }
         }
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
 
-        while(j != n) {
-            merged[k++] = nums2[j++];
+    public static void merge(int[] MERGED, int[] L, int[] R) {
+        int i,j,k;
+        i=j=k=0;
+        while(i < L.length && j < R.length) {
+            if(L[i] < R[j]) {
+                MERGED[k++] = L[i++];
+            }
+            else {
+                MERGED[k++] = R[j++];
+            }
         }
-*/
+        while (i < L.length) {
+            MERGED[k++] = L[i++];
+        }
+        while (j < R.length) {
+            MERGED[k++] = R[j++];
+        }
+    }
+}

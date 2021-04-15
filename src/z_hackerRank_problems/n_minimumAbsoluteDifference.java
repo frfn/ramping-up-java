c4d6a6248d9daf4637dcac362559df233487da33
frfn <flexer.fadrigalan@gmail.com>
2021-04-14 20:19:00 -0400

    finished mySqRt problem, use binary search approach | minimumAbsoluteDifference is a greedy algorithm which demonstrates that sometimes, having a sorted array will produce a faster approach to solving a issue

---
+++ src/z_hackerRank_problems/n_minimumAbsoluteDifference.java
@@ -0,0 +1,49 @@
+package z_hackerRank_problems;
+
+import java.util.Arrays;
+
+public class n_minimumAbsoluteDifference {
+
+    public static void main(String[] args) {
+        System.out.println(minimumAbsoluteDifference(new int[] {1, -3, 71, 68, 17}));
+
+    }
+
+    public static int minimumAbsoluteDifference(int[] arr) {
+        Arrays.sort(arr); // by sorting this array, we will tackle all possible values
+
+        int minimum = Integer.MAX_VALUE;
+
+        // -3, 1, 17, 68, 71
+
+        for(int i = 0; i < arr.length - 1; i++) {
+            minimum = Math.min(Math.abs(arr[i] - arr[i+1]), minimum);
+        }
+
+        return minimum;
+    }
+
+    public static int minimumAbsoluteDifferenceNonGreedy(int[] arr) {
+
+        // works, but not greedy
+
+        int minimum = Math.abs(Integer.MAX_VALUE);
+
+        for(int i = 0; i < arr.length; i++) {
+
+            for(int j = i + 1; j < arr.length; j++) {
+
+                int absoluteCalculation = Math.abs(arr[j] - arr[i]);
+
+                System.out.println(arr[i] + " " + arr[j] + " : " + absoluteCalculation);
+
+                minimum = Math.min(absoluteCalculation, minimum);
+
+            }
+
+        }
+        return minimum;
+
+    }
+
+}
---
+++ src/z_leetCode_problems/q_mySqRt.java
@@ -0,0 +1,46 @@
+package z_leetCode_problems;
+
+public class q_mySqRt {
+
+    // you're practically looking in a sorted list, you can use binary search!
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(69));
+    }
+
+    public static int binarySearch(int x) {
+        if(x < 2) return x;
+
+        int left = 2;
+        int right = x / 2; // divide by two, kickstarting the logic as well as handling overflowed numbers!
+        while(left <= right) {
+            int middle = (left + right) / 2;
+
+            long result = (long) middle * middle; // evaluation
+
+            if(result > x) {
+                right = middle - 1;
+            }
+            else if (result < x ){
+                left = middle + 1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return right;
+    }
+
+    public static int mySqRt(int x) {
+            long counter = 1;
+
+            while(counter <= x) {
+                if(counter * counter > x) {
+                    return (int)counter - 1;
+                }
+                counter++;
+            }
+            return (int)counter - 1;
+    }
+
+}
